3d16de3080bda904642f51bfc9107ec9bc6bd97b
Yvan Pearson <ypearson@nklabs.com>
2017-06-17 20:50:42 -0400

    this is the first commit

---
+++ test1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+a=["one", "two", "three","four","five","six","seven","eight","nine"]
+b=["eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+c=["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+d=["hundred","thousand"]
+
+
+
+3421
+print(a[2]+" "+d[1]+" "+a[3]+" "+d[0]+" "+c[1]+" "+a[0])
+
+
